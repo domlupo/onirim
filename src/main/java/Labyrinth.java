a78adf14409e20067c7c8294dea60840ae9d46fc
Dominic <dominiclupo5318@gmail.com>
2022-05-07 21:28:04 -0700

    Add Labyrinth playCard method

--- src/main/java/Labyrinth.java
+++ src/main/java/Labyrinth.java
@@ -2,10 +2,45 @@ import java.util.ArrayList;
 
 public class Labyrinth {
 
-    private ArrayList<Card> labyrinth = new ArrayList<Card>();
+    private ArrayList<Card> labyrinth;
+
+    public Labyrinth() {
+        labyrinth = new ArrayList<>();
+    }
+
+    public Labyrinth(Card card) {
+        labyrinth = new ArrayList<>();
+        labyrinth.add(card);
+    }
+
+    public Labyrinth(ArrayList<Card> cards) {
+        labyrinth = new ArrayList<>(cards);
+    }
 
     public ArrayList<Card> getLabyrinth() {
         return labyrinth;
+    }
+
+    public void playCard(Card playCard) throws OnirimException {
+        if (playCard.getSymbol() == Card.Symbol.DOOR ||
+                playCard.getSymbol() == Card.Symbol.NIGHTMARE) {
+            throw new OnirimException("Could not play card because the card symbol is  " +
+                    playCard.getSymbol() + " which can never be played in labyrinth.");
+        }
+
+        if (labyrinth.size() == 0) {
+            labyrinth.add(playCard);
+            return;
+        }
+
+        Card lastLabyrinthCard = labyrinth.get(labyrinth.size() - 1);
+
+        if (lastLabyrinthCard.getSymbol() == playCard.getSymbol()) {
+            throw new OnirimException("Could not play card because the card symbol " + playCard.getSymbol() +
+                    " is the last played symbol in the labyrinth.");
+        }
+
+        labyrinth.add(playCard);
     }
 
     public String toString() {
---
+++ src/test/java/LabyrinthTest.java
@@ -0,0 +1,104 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabyrinthTest {
+
+    @Nested
+    class playCard {
+        @Test
+        void playCardEmptyLabyrinth() throws OnirimException {
+            Card playCard = new Card.Builder()
+                    .setColor(Card.Color.RED)
+                    .setSymbol(Card.Symbol.SUN)
+                    .build();
+
+            Labyrinth labyrinth = new Labyrinth();
+            labyrinth.playCard(playCard);
+
+            assertTrue(labyrinth.getLabyrinth().size() == 1);
+            assertTrue(labyrinth.getLabyrinth().contains(playCard));
+        }
+
+        @Test
+        void playValidCardWithDifferentSymbol() throws OnirimException {
+            Card.Color color = Card.Color.RED;
+            Card.Symbol playCardSymbol = Card.Symbol.SUN;
+            Card.Symbol labyrinthCardSymbol = Card.Symbol.MOON;
+
+            Card playCard = new Card.Builder()
+                    .setColor(color)
+                    .setSymbol(playCardSymbol)
+                    .build();
+            Card labyrinthCard = new Card.Builder()
+                    .setColor(color)
+                    .setSymbol(labyrinthCardSymbol)
+                    .build();
+
+            Labyrinth labyrinth = new Labyrinth(labyrinthCard);
+
+            assertTrue(labyrinth.getLabyrinth().size() == 1);
+            assertTrue(labyrinth.getLabyrinth().contains(labyrinthCard));
+            labyrinth.playCard(playCard);
+            assertTrue(labyrinth.getLabyrinth().size() == 2);
+            assertTrue(labyrinth.getLabyrinth().contains(labyrinthCard));
+            assertTrue(labyrinth.getLabyrinth().contains(playCard));
+        }
+
+        @Test
+        void playInvalidCardWithMatchingSymbol() {
+            Card.Color color = Card.Color.RED;
+            Card.Symbol matchingSymbol = Card.Symbol.SUN;
+
+            Card playCard = new Card.Builder()
+                    .setColor(color)
+                    .setSymbol(matchingSymbol)
+                    .build();
+            Card labyrinthCard = new Card.Builder()
+                    .setColor(color)
+                    .setSymbol(matchingSymbol)
+                    .build();
+
+            Labyrinth labyrinth = new Labyrinth(labyrinthCard);
+
+            assertTrue(labyrinth.getLabyrinth().size() == 1);
+            assertTrue(labyrinth.getLabyrinth().contains(labyrinthCard));
+            OnirimException thrown = assertThrows(
+                    OnirimException.class,
+                    () -> labyrinth.playCard(playCard),
+                    "Expected playCard(playCard) to throw, but it didn't"
+            );
+
+            assertTrue(labyrinth.getLabyrinth().size() == 1);
+            assertTrue(labyrinth.getLabyrinth().contains(labyrinthCard));
+            assertTrue(thrown.getMessage().contains("Could not play card because the card symbol " + playCard.getSymbol() +
+                    " is the last played symbol in the labyrinth."));
+        }
+
+
+
+        @ParameterizedTest
+        @EnumSource(value = Card.Symbol.class, names = {"DOOR", "NIGHTMARE"})
+        void playInvalidCardWithMatchingSymbol(Card.Symbol symbol) {
+            Card playCard = new Card.Builder()
+                    .setSymbol(symbol)
+                    .build();
+            Labyrinth labyrinth = new Labyrinth();
+
+            OnirimException thrown = assertThrows(
+                    OnirimException.class,
+                    () -> labyrinth.playCard(playCard),
+                    "Expected playCard(playCard) to throw, but it didn't"
+            );
+
+            assertTrue(labyrinth.getLabyrinth().size() == 0);
+            assertTrue(thrown.getMessage().contains("Could not play card because the card symbol is  " +
+                    playCard.getSymbol() + " which can never be played in labyrinth."));
+        }
+    }
+
+}
