6161d9852a95dfb9f649363ba99a1f5e7f171eb1
Dominic <dominiclupo5318@gmail.com>
2022-06-04 21:59:27 -0700

    Fix Limbo exception

--- src/main/java/Limbo.java
+++ src/main/java/Limbo.java
@@ -24,10 +24,6 @@ public class Limbo {
     public void addCard(Card card) throws OnirimException {
         if (card.getSymbol() == Card.Symbol.DOOR || card.getSymbol() == Card.Symbol.NIGHTMARE) {
             limbo.add(card);
-        } else {
-            throw new OnirimException(
-                    String.format("Could not limbo card because the card symbol is %s.", card.getSymbol())
-            );
         }
     }
 
